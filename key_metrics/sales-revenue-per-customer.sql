1dc245059e8062e69c7f132aa29e95576bf40b35
putrimd <144097289+putrimd@users.noreply.github.com>
2025-01-15 14:51:50 +0700

    Update sales-revenue-per-customer.sql

--- key_metrics/sales-revenue-per-customer.sql
+++ key_metrics/sales-revenue-per-customer.sql
@@ -1,4 +1,6 @@
--- Sales Per Customer
-SELECT corrected_customer_id, SUM(Sales) AS Customer_Revenue
-FROM OrderDetails
-GROUP BY corrected_customer_id;
+-- Sales Revenue Per Customer
+SELECT od.corrected_customer_id, c.Customer_Name, SUM(od.sales) as Total_Sales_Revenue
+FROM OrderDetails od
+inner join customers c on od.corrected_customer_id = c.corrected_customer_id
+group by od.corrected_customer_id, c.customer_name
+order by Total_Sales_Revenue desc --Data sorted by greater revenue per customer
